4b063e903d38dc639497f4cfd1f050f0df8eff6c
Brandon Jordan <brandonjordan124@gmail.com>
2025-09-26 20:58:21 -0400

    Add newer iOS and macOS host versions

--- Sources/Open-Jellycore/Core/Compiler/Shortcuts Equivelent Models/ShortcutsHostVersion.swift
+++ Sources/Open-Jellycore/Core/Compiler/Shortcuts Equivelent Models/ShortcutsHostVersion.swift
@@ -11,8 +11,14 @@ public enum ShortcutsHostVersion: Int, Encodable, CaseIterable {
     case iOS14
     case iOS15
     case iOS16
+    case iOS17
+    case iOS18
+    case iOS26
     case macOS12
-    
+    case macOS13
+    case macOS14
+    case macOS26
+
     /// The custom version number. Used for detecting if versions are less than one or the other.
     public var version: Int {
         switch self {
@@ -24,8 +30,20 @@ public enum ShortcutsHostVersion: Int, Encodable, CaseIterable {
             return 2
         case .iOS16:
             return 3
+        case .iOS17:
+            return 4
+        case .iOS18:
+            return 5
+        case .iOS26:
+            return 6
         case .macOS12:
             return 3
+        case .macOS13:
+            return 4
+        case .macOS14:
+            return 5
+        case .macOS26:
+            return 6
         }
     }
     
@@ -40,8 +58,20 @@ public enum ShortcutsHostVersion: Int, Encodable, CaseIterable {
             return "iOS 15"
         case .iOS16:
             return "iOS 16"
+        case .iOS17:
+            return "iOS 17"
+        case .iOS18:
+            return "iOS 18"
+        case .iOS26:
+            return "iOS 26"
         case .macOS12:
             return "macOS12"
+        case .macOS13:
+            return "macOS13"
+        case .macOS14:
+            return "macOS14"
+        case .macOS26:
+            return "macOS26"
         }
     }
 }
